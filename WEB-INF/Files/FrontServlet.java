a25e85da25a13fc8bee8460e3f81d41407fcef8a
DylanQin4 <dylanqin4@gmail.com>
2023-03-06 01:08:33 +0100

    get url

---
+++ WEB-INF/Files/FrontServlet.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class FrontServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String url=request.getServletPath();
+        String requete=request.getQueryString();
+        if (requete!=null) {
+            url=url+"?"+requete;
+        }
+        request.setAttribute("url",url);
+        RequestDispatcher dispat=request.getRequestDispatcher("url.jsp");
+        dispat.forward(request,response);
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
---
+++ index.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> fs </title>
+</head>
+<body>
+	<h1> Page accueil vers meme servlet </h1>
+	<form action="FrontServlet" method="GET"> 
+		input 1:<input type="text" name="i1"> </input>
+		input 2:<input type="text" name="i2"> </input>
+		<input type="submit" value="Insert"> </body>
+	</form>
+</body>
+</html>
